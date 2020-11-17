e218e339746d8589248303645356678344f626dd
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-17 14:20:41 +0000

    Add files via upload

---
+++ #betweenwalls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betweenwalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int rn1 = r.Next(1, 101);
+            int rn2 = r.Next(1, 101);
+
+
+            if(rn1 > rn2) { for (int i = rn2; i < rn1; i++) {
+                    Console.WriteLine(i); } }
+
+            if (rn2 > rn1)
+            {
+                for (int i = rn1; i < rn2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
