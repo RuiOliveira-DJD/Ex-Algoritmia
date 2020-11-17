a954be7c5ee4e5b6428778cd7bc6c28aeac5c626
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-17 15:10:32 +0000

    Add files via upload

---
+++ #Filli_n.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filli_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valoresA = new int[10]; //////////////////////////a)
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                valoresA[i] = i + 1;
+            }
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(valoresA[i]);
+            }
+
+
+
+
+            int[] valoresB = new int[10]; //////////////////////////b)           
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 5)
+                {
+                    valoresB[i] = i + 1;
+                }
+                if (i >= 5)
+                    valoresB[i] = (i - 4) * 10;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(valoresB[i]);
+            }
+
+
+
+
+            int[] valoresC = new int[11]; //////////////////////////c)
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (i >= 2)
+                {
+                    valoresC[i] = i * i;
+                }
+                else
+                {
+                    valoresC[i] = i;
+                }
+            }
+           
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(valoresC[i]);
+            }          
+
+            }
+        }
+    }
+
+
