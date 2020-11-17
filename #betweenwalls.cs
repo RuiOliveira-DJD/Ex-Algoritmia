327ed093d352374e2f47d5884ab6b1871badc689
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-17 14:23:05 +0000

    Update #betweenwalls.cs

--- #betweenwalls.cs
+++ #betweenwalls.cs
@@ -16,12 +16,12 @@ namespace Betweenwalls
             int rn2 = r.Next(1, 101);
 
 
-            if(rn1 > rn2) { for (int i = rn2; i < rn1; i++) {
+            if(rn1 > rn2) { for (int i = rn2; i <= rn1; i++) {
                     Console.WriteLine(i); } }
 
             if (rn2 > rn1)
             {
-                for (int i = rn1; i < rn2; i++)
+                for (int i = rn1; i <= rn2; i++)
                 {
                     Console.WriteLine(i);
                 }
