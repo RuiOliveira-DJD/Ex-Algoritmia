3fe7f8c01c0146819cd0bfb3dbd16ae538679f29
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-18 18:12:51 +0000

    Add files via upload

---
+++ #leaderboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int p1 = r.Next(1, 100);
+            int p2 = r.Next(1, 100);
+            int p3 = r.Next(1, 100);
+
+
+
+            int maior1 = p1;
+            int maior2 = p2;
+            int maior3 = p3;
+
+            if(p2 > maior1)
+            {
+                maior2 = maior1;
+                maior1 = p2;
+            } 
+
+            if(p3 > maior1)
+            {
+                maior3 = maior2;
+                maior2 = maior1;
+                maior1 = p3;
+            }
+           
+            else
+            {
+                if(p3 > maior2)
+                {
+                    maior3 = maior2;
+                    maior2 = p3;
+                }
+            }
+            
+
+        }
+    }
+}
