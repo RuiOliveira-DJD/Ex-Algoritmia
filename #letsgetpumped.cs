6b57b50fc5c588b13afa54b5f58a0fe9dc134c6a
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-22 18:18:22 +0000

    Add files via upload

---
+++ #letsgetpumped.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letsgetpumped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            float attack = r.Next(0, 50);
+
+            float powerup = attack / 2;
+
+            float poweredupattack = attack + powerup;
+
+            Console.WriteLine("Normal attack = " + attack);
+            Console.WriteLine("Powered UP attack = " + poweredupattack);
+        }
+    }
+}
