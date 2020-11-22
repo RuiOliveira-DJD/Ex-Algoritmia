03abe172877df8fd5c6b34b4071d6189186d8fe1
RuiOliveira-DJD <73547419+RuiOliveira-DJD@users.noreply.github.com>
2020-11-22 18:21:58 +0000

    Add files via upload

---
+++ #timeover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 99; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                if (i == 0) Console.WriteLine("Time Over");
+            }
+        }
+    }
+}
